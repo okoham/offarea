56f23fcd5b3fe29d46042db8be8abc40e54e4dce
Oliver Koch <n52e09@gmail.com>
2020-11-07 19:17:47 +0100

    remove linked list; use gnu11 to make getline work

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 P = offarea
 OBJECTS = offarea.o 
-CFLAGS = -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror \
+CFLAGS = -std=gnu11 -ggdb3 -O0 -Qunused-arguments \
+		 -Wall -Werror \
          -Wextra -Wno-sign-compare -Wno-unused-parameter \
 		 -Wno-unused-variable -Wshadow
 LDLIBS = -lm
 CC = clang
 
 $(P): $(OBJECTS)
+
--- offarea.c
+++ offarea.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 
-typedef struct 
+typedef struct point
 {
     double x;
     double y;
@@ -14,18 +14,8 @@ typedef struct
 point;
 
 
-// könnte das auch einfacher machen: pro zeile ein array mit malloc
-// länge des arrays ist ja bekannt
-typedef struct vertex
-{
-    int id;
-    struct vertex *next;
-} 
-vertex;
-
-
 // das geht auch ...
-typedef struct 
+typedef struct face
 {
     int nv;
     int *vertices;
@@ -33,114 +23,12 @@ typedef struct
 face;
 
 
-// triangle area
-double dummy_triangle_area(point v0, point v1, point v2)
-{
-    return 0.1234;
-}
+// function declarations
+double triangle_area(point v0, point v1, point v2);
+void parse_line2_tok(char *line, int *nv, int *nf);
+void parse_coords(char *line, point *p);
+void parse_face(char *line, face *f);
 
-
-double triangle_area(point v0, point v1, point v2)
-{
-    double tz = (v1.x * v0.y) - (v0.x * v1.y) 
-             + (v2.x * v1.y) - (v1.x * v2.y)
-             + (v0.x * v2.y) - (v2.x * v0.y);
-    double ty = (v1.x * v0.z) - (v0.x * v1.z) 
-             + (v2.x * v1.z) - (v1.x * v2.z)
-             + (v0.x * v2.z) - (v2.x * v0.z);
-    double tx = (v1.y * v0.z) - (v0.y * v1.z) 
-             + (v2.y * v1.z) - (v1.y * v2.z)
-             + (v0.y * v2.z) - (v2.y * v0.z);
-    return 0.5 * sqrt(tx*tx + ty*ty + tz*tz);
-}
-
-
-
-void parse_line2_tok(char *line, int *nv, int *nf)
-{
-    char *delim = " ";
-    *nv = atoi(strtok(line, delim));
-    *nf = atoi(strtok(NULL, delim));
-}
-
-
-// get the x, y, z coordinates
-void parse_coords(char *line, point *p)
-{
-    // char *token = NULL;
-    char *delim = " ";
-    p->x = atof(strtok(line, delim)); 
-    p->y = atof(strtok(NULL, delim)); 
-    p->z = atof(strtok(NULL, delim)); 
-}
-
-// parse the vertices into an array? 
-void parse_face(char *line, face *f)
-{
-    char *delim = "\n ";
-    size_t size = sizeof(int);
-
-    // number of vertices
-    char *token = strtok(line, delim);
-    int nv = atoi(token);
-
-    // allocate memory
-    int *vertex_indices = calloc(size, nv);
-    if (vertex_indices == NULL)
-    {
-        exit(1);
-    }
-
-    // loop over remaining items
-    int *p = vertex_indices;
-    for (int i = 0; i < nv; i++, p += sizeof(int))
-    {
-        token = strtok(NULL, delim);
-        *p = atoi(token);
-    }
-
-    // build struct
-    f->nv = nv;
-    f->vertices = vertex_indices;
-}
-
-
-// parse the vertices into a linked list
-// here, we ignore the provided information of the number of vertices.
-vertex *parse_face_list(char* line)
-{
-    // space and newline
-    char *delim = "\n ";
-    // initialise list
-    vertex *list = NULL;
-    // first element is num vertices. not used.
-    char *token = strtok(line, delim);
-
-    // this stores the vertexes in revesed order.
-    while ((token = strtok(NULL, delim)) != NULL)
-    {
-        vertex *v = malloc(sizeof(vertex));
-        if (v == NULL)
-        {
-            exit(1);
-        }
-        v->id = atoi(token);
-        v->next = list;
-        list = v;
-    }
-    return list;
-}
-
-
-// free vertex lists. recursively
-void free_list(vertex *list)
-{
-    if (list != NULL)
-    {
-        free_list(list->next);
-        free(list);
-    }
-}
 
 
 int main(int argc, char *argv[])
@@ -247,37 +135,82 @@ int main(int argc, char *argv[])
     {
         free(faces[i].vertices);
     }
-
-
-
     
     // close file
     fclose(infile);
 
     free(line);
     free(vertices);
-
-
-    // write a function that frees the linked list
-    
-
-    // read first line
-    // make sure it starts with OFF and two numbers
-    // handle freak cases with missing newline
-
-    // allocate memory for arrays
-
-    // loop over nodes
-    // return error if not proper format
-    // return error if not expected number of lines
-
-    // loop over elements
-    // return error if incorrect format or number of lines not meeting expectations
-
-    // compute area
-    // print area
-
-    // free memory
-
     return 0;
 }
+
+
+// compute area of a triagle in 3d space
+double triangle_area(point v0, point v1, point v2)
+{
+    double tz = (v1.x * v0.y) - (v0.x * v1.y) 
+             + (v2.x * v1.y) - (v1.x * v2.y)
+             + (v0.x * v2.y) - (v2.x * v0.y);
+    double ty = (v1.x * v0.z) - (v0.x * v1.z) 
+             + (v2.x * v1.z) - (v1.x * v2.z)
+             + (v0.x * v2.z) - (v2.x * v0.z);
+    double tx = (v1.y * v0.z) - (v0.y * v1.z) 
+             + (v2.y * v1.z) - (v1.y * v2.z)
+             + (v0.y * v2.z) - (v2.y * v0.z);
+    return 0.5 * sqrt(tx*tx + ty*ty + tz*tz);
+}
+
+
+// parse line containing number of vertices, faces, edges.
+// number of edges is ignored
+void parse_line2_tok(char *line, int *nv, int *nf)
+{
+    char *delim = " ";
+    *nv = atoi(strtok(line, delim));
+    *nf = atoi(strtok(NULL, delim));
+} 
+
+
+
+// get the x, y, z coordinates
+void parse_coords(char *line, point *p)
+{
+    // char *token = NULL;
+    char *delim = " ";
+    p->x = atof(strtok(line, delim)); 
+    p->y = atof(strtok(NULL, delim)); 
+    p->z = atof(strtok(NULL, delim)); 
+}
+
+// parse the vertices into an array? 
+void parse_face(char *line, face *f)
+{
+    char *delim = "\n ";
+    size_t size = sizeof(int);
+
+    // number of vertices
+    char *token = strtok(line, delim);
+    int nv = atoi(token);
+
+    // allocate memory
+    int *vertex_indices = calloc(size, nv);
+    if (vertex_indices == NULL)
+    {
+        exit(1);
+    }
+
+    // loop over remaining items
+    int *p = vertex_indices;
+    for (int i = 0; i < nv; i++, p += sizeof(int))
+    {
+        token = strtok(NULL, delim);
+        *p = atoi(token);
+    }
+
+    // build struct
+    f->nv = nv;
+    f->vertices = vertex_indices;
+}
+
+
+
